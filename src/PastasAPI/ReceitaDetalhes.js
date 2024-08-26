8a8110c2cf307e96f3e0dbee9b38de66b2cb4b89
Nicolas-Marcelo <86321896+Nicolas-Marcelo@users.noreply.github.com>
2024-08-26 16:38:09 -0300

    Update ReceitaDetalhes.js

--- src/PastasAPI/ReceitaDetalhes.js
+++ src/PastasAPI/ReceitaDetalhes.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import axios from "axios";
+import './ReceitaDetalhes.css';
 
 const ReceitaDetalhes = () => {
-    const { id } = useParams(); // Obtém o ID da URL
+    const { id } = useParams();
     const [post, setPost] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('/api.json')
             .then(response => {
-                console.log("Dados recebidos:", response.data); // Verifique os dados recebidos
+                console.log("Dados recebidos:", response.data);
                 const selectedPost = response.data.find(p => p.id === parseInt(id, 10));
-                console.log("Post selecionado:", selectedPost); // Verifique o post selecionado
+                console.log("Post selecionado:", selectedPost);
                 setPost(selectedPost);
                 setLoading(false);
             })
@@ -27,27 +29,30 @@ const ReceitaDetalhes = () => {
     if (!post) return <p>Post não encontrado...</p>;
 
     return (
-        <div>
-            <h1>Pagina de detalhes</h1>
-            <h1>{post.nome}</h1>
-            <h3>{post.categoria}</h3>
-            <h3>{post.tempo_preparo}</h3>
-            <h3>{post.rendimento}</h3>
-            <ul>
-                {post.igredientes.map((igredientes, index) => (
-                    <li key={index}>
-                        <input type="checkbox" />
-                        {igredientes}
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {post.modopreparo.map((modopreparo, index) => (
-                    <li key={index}>{modopreparo}</li>
-                ))}
-            </ul>
+        <div className="detalhe-caixa">
+            <h1 className="detalhe-nome">{post.nome}</h1>
+            <div className="detalhe-info">
+                <img src={post.img} alt="imagem da receita" className="detalhe-imagem" />
+                <p className="detalhe-descricao">{post.descricao}</p>
+            </div>
+            <div className="detalhe-passos">
+                <ul className="detalhe-ingrediente">
+                    {post.igredientes.map((igredientes, index) => (
+                        <li key={index}>
+                            <input type="checkbox" />
+                            {igredientes}
+                        </li>
 
-            <a href="/">Ver todas as receitas</a>
+                    ))}
+                </ul>
+                <ul className="detalhe-preparo">
+                    {post.modopreparo.map((modopreparo, index) => (
+                        <li key={index}>{modopreparo}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <Link to="/" className="detalhe-link">Ver todas as receitas</Link>
         </div>
     );
 }
